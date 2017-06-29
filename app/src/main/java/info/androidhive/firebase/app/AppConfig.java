b452d97ebb7a359590447613e0b5bd63d4ce122e
neha1671993 <neha1671993@gmail.com>
2017-06-29 10:03:01 +0530

    First Commit of Job refers

---
+++ app/src/main/java/info/androidhive/firebase/app/AppConfig.java
@@ -0,0 +1,9 @@
+package info.androidhive.firebase.app;
+
+public class AppConfig {
+	// Server user login url
+	public static String URL_LOGIN = "http://localhost/android_login_api/login.php";
+
+	// Server user register url
+	public static String URL_REGISTER = "http://localhost/android_login_api/register.php";
+}
